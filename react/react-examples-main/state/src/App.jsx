caa17ccf8f0c2f2bdeb3979450c86036f3738d5e
MarcosDebona25 <marcosjd2005@gmail.com>
2026-08-11 20:49:58 +0000

    chore: agregar ejemplos de React de The Odin Project

---
+++ react/react-examples-main/state/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+
+const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [count, setCount] = useState(0);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <div
+        className="App"
+        style={{
+          backgroundColor,
+        }}
+      >
+        {COLORS.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={onButtonClick(color)}
+            className={backgroundColor === color ? 'selected' : ''}
+          >
+            {color}
+          </button>
+        ))}
+        <b>{count}</b>
+      </div>
+    </>
+  );
+}
+
+export default App;
